b3f88c97ec18c87287dff3a35de08e0dfd29e45c
Damien <damien.pellet94@hotmail.fr>
2022-04-27 15:34:51 +0200

    ajout const elt de form et essaie de function validate pour form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,19 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll('.close');
 
+// Form element const
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const eMail = document.getElementById('email');
+const birthDate = document.getElementById('birthdate');
+const loc1 = document.getElementById('location1');
+const loc2 = document.getElementById('location2');
+const loc3 = document.getElementById ('location3');
+const loc4 = document.getElementById ('location4');
+const loc5 = document.getElementById ('location5');
+const loc6 = document.getElementById ('location6');
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -22,10 +35,16 @@ function launchModal() {
 }
 
 // close modal form
-
 closeModalBtn.forEach(close => close.addEventListener("click", closeModal));
 
 
 function closeModal(){
   modalbg.style.display = 'none';
+}
+
+// Input firstName
+function validate(){
+  if (firstName === '' && firstName.lenght > 1 ) {
+    return false;
+  }
 }
